ea59e70a31112df9d37179e4932cb799e8c05097
Vyacheslav Daradur <daradurvs@gmail.com>
2017-05-18 17:18:03 +0300

    WIP: JMH benchmark

--- src/main/java/daradurvs/ru/ignite/compression/ThroughputBenchmark.java
+++ src/main/java/daradurvs/ru/ignite/compression/ThroughputBenchmark.java
@@ -17,32 +17,105 @@
 
 package daradurvs.ru.ignite.compression;
 
+import daradurvs.ru.ignite.compression.model.Audit;
+import daradurvs.ru.ignite.compression.model.Audit1F;
+import daradurvs.ru.ignite.compression.model.Identifiable;
+import daradurvs.ru.ignite.compression.model.ModelFactory;
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
+import org.apache.ignite.IgniteCheckedException;
+import org.apache.ignite.Ignition;
+import org.apache.ignite.marshaller.Marshaller;
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
+import org.openjdk.jmh.annotations.Warmup;
 
+import static daradurvs.ru.ignite.compression.model.DataGenerator.AUDIT_CSV;
+
+@BenchmarkMode({Mode.Throughput, Mode.AverageTime})
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
 public class ThroughputBenchmark {
-    /*
-    public static void main(String[] args) {
+    private static final ClassLoader CLASS_LOADER = ThroughputBenchmark.class.getClassLoader();
 
+    @State(Scope.Thread)
+    public static class IgniteState {
+        public Ignite ignite;
+        public IgniteCache cache;
+        public Marshaller marshaller;
+        public List<Identifiable> audits;
+        public List<Identifiable> audits1F;
+
+        @Setup(Level.Trial)
+        public void setUp() throws IOException {
+            ignite = Ignition.start(CLASS_LOADER.getResourceAsStream("cache-config-snappy.xml"));
+            cache = ignite.createCache(Long.toString(System.currentTimeMillis()));
+            marshaller = ignite.configuration().getMarshaller();
+            audits = ModelFactory.create(Audit.class, AUDIT_CSV);
+            audits1F = ModelFactory.create(Audit1F.class, AUDIT_CSV);
+        }
+
+        @TearDown(Level.Trial)
+        public void teatDown() {
+            cache.destroy();
+            ignite.close();
+        }
     }
-
-    private Ignite ignite;
-
-    private IgniteCache cache;
 
     @Benchmark
-    public Object get() {
+    @Measurement(iterations = 20)
+    @Warmup(iterations = 20)
+    @Fork(1)
+    public byte[] marshal(IgniteState state) throws IgniteCheckedException {
         // filled cache
         // ids of objects
-
-        return null;
+        return state.marshaller.marshal(state.audits.get(0));
     }
 
+    @Benchmark
+    @Measurement(iterations = 20)
+    @Warmup(iterations = 20)
+    @Fork(1)
+    public byte[] marshal1F(IgniteState state) throws IgniteCheckedException {
+        // filled cache
+        // ids of objects
+        return state.marshaller.marshal(state.audits1F.get(0));
+    }
+/*
+    @Benchmark
+    public Object get(IgniteState state) throws IgniteCheckedException {
+        // filled cache
+        // ids of objects
+        return state.marshaller.marshal(state.audits.get(0));
+    }
+
+    @Benchmark
+    public Object get1F(IgniteState state) throws IgniteCheckedException {
+        // filled cache
+        // ids of objects
+        return state.marshaller.marshal(state.audits1F.get(0));
+    }
+/*
     @Benchmark
     public void put() {
         // objects
         // empty cache
     }
-    */
+
+    @Benchmark
+    public void put1F() {
+        // objects
+        // empty cache
+    }*/
 }
