a489683bdc7e2f3db7f2c4549061e5f07a8569f7
Vyacheslav Daradur <daradurvs@gmail.com>
2017-05-19 13:14:57 +0300

    JMH benchmarks were added

---
+++ src/main/java/daradurvs/ru/ignite/compression/jmh/CacheUnmarshallJmhBenchmark.java
@@ -0,0 +1,117 @@
+package daradurvs.ru.ignite.compression.jmh;
+
+import daradurvs.ru.ignite.compression.model.Audit;
+import daradurvs.ru.ignite.compression.model.Audit1F;
+import java.io.IOException;
+import org.apache.ignite.IgniteCheckedException;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import static daradurvs.ru.ignite.compression.model.DataGenerator.AUDIT_CSV;
+
+public class CacheUnmarshallJmhBenchmark extends AbstractJmhBenchmark {
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+            .include(CacheUnmarshallJmhBenchmark.class.getSimpleName())
+            .warmupIterations(20)
+            .measurementIterations(50)
+            .forks(1)
+            .jvmArgs("-ea", "-Xms4g", "-Xmx4g")
+            .shouldFailOnError(true)
+            .build();
+
+        new Runner(opt).run();
+    }
+
+    // "put" operation without compression
+    @State(Scope.Thread)
+    public static class IgniteCacheState {
+        IgniteLazyState i;
+        byte[] arr;
+
+        @Setup(Level.Iteration)
+        public void prepare() throws IgniteCheckedException {
+            arr = i.marshaller.marshal(i.entry);
+        }
+
+        @Setup(Level.Trial)
+        public void setUp() throws IOException {
+            i = new IgniteLazyState(Audit.class, "cache-config-deflater.xml", AUDIT_CSV);
+            i.init();
+        }
+
+        @TearDown(Level.Trial)
+        public void teatDown() {
+            i.destroy();
+        }
+    }
+
+    @Benchmark
+    public Object unmarshall(IgniteCacheState state) throws IgniteCheckedException {
+        return state.i.marshaller.unmarshal(state.arr, state.i.ldr);
+    }
+
+    // "put" operation with deflater compression
+    @State(Scope.Thread)
+    public static class IgniteCacheDeflaterState {
+        IgniteLazyState i;
+        byte[] arr;
+
+        @Setup(Level.Iteration)
+        public void prepare() throws IgniteCheckedException {
+            arr = i.marshaller.marshal(i.entry);
+        }
+
+        @Setup(Level.Trial)
+        public void setUp() throws IOException {
+            i = new IgniteLazyState(Audit1F.class, "cache-config-deflater.xml", AUDIT_CSV);
+            i.init();
+        }
+
+        @TearDown(Level.Trial)
+        public void teatDown() {
+            i.destroy();
+        }
+    }
+
+    @Benchmark
+    public Object unmarshallWithDeflater(IgniteCacheDeflaterState state) throws IgniteCheckedException {
+        return state.i.marshaller.unmarshal(state.arr, state.i.ldr);
+    }
+
+    // "put" operation with snappy compression
+    @State(Scope.Thread)
+    public static class IgniteCacheSnappyState {
+        IgniteLazyState i;
+        byte[] arr;
+
+        @Setup(Level.Iteration)
+        public void prepare() throws IgniteCheckedException {
+            arr = i.marshaller.marshal(i.entry);
+        }
+
+        @Setup(Level.Trial)
+        public void setUp() throws IOException {
+            i = new IgniteLazyState(Audit1F.class, "cache-config-snappy.xml", AUDIT_CSV);
+            i.init();
+        }
+
+        @TearDown(Level.Trial)
+        public void teatDown() {
+            i.destroy();
+        }
+    }
+
+    @Benchmark
+    public Object unmarshallWithSnappy(IgniteCacheSnappyState state) throws IgniteCheckedException {
+        return state.i.marshaller.unmarshal(state.arr, state.i.ldr);
+    }
+}
